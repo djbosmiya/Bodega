8f57a2e552f54d0815e7645e05e8f75987fd78a7
djbosmiya <djbosmiya@gmail.com>
2024-08-21 10:28:28 -0400

    changes done to navbar.jsx in develop branch

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -10,7 +10,7 @@ import { useContext } from 'react';
 import { ShopContext } from '../../Context/ShopContext';
 import { useState } from 'react';
 
-//changes done to navbar.jsx for the checking develop branch
+//changes done to navbar.jsx for the checking develop branch 
 const MyNavbar = () => {
 
     // const [menu, setMenu] = useState("home");
